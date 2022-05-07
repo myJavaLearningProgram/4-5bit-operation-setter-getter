785d0035ba9a56f6301f1e725e4f47d8d7892d71
Ories <tellt1@126.com>
2022-05-07 15:11:09 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Java的运算系统练习：使用位运算访问属性
+
+请在`Cat`类中按照要求编写指定方法，使用位运算读写属性。
+
+在提交Pull Request之前，你应当在本地确保所有代码已经编译通过，并且通过了测试(`mvn clean verify`)
+
+-----
+注意！我们只允许你修改以下文件，对其他文件的修改会被拒绝：
+- src/main/java/com/github/hcsp/calculation/Cat.java
+-----
+
---
+++ src/test/java/com/github/hcsp/calculation/CatTest.java
@@ -0,0 +1,79 @@
+package com.github.hcsp.calculation;
+
+import java.lang.reflect.Field;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CatTest {
+    @Test
+    public void test1() throws Exception {
+        Cat cat = new Cat();
+        cat.setCute(true);
+        cat.setCute(true);
+        cat.setFat(false);
+        cat.setWhite(false);
+        cat.setWhite(true);
+        Assertions.assertTrue(cat.isCute());
+        Assertions.assertFalse(cat.isFat());
+        Assertions.assertTrue(cat.isWhite());
+
+        Field field = Cat.class.getDeclaredField("properties");
+        field.setAccessible(true);
+        Assertions.assertEquals(0x1 | 0x4, field.get(cat));
+    }
+
+    @Test
+    public void test2() throws Exception {
+
+        Cat cat = new Cat();
+        for (int i = 0; i < 100; ++i) {
+            cat.setCute(true);
+            cat.setFat(true);
+            cat.setWhite(true);
+        }
+        Assertions.assertTrue(cat.isCute());
+        Assertions.assertTrue(cat.isFat());
+        Assertions.assertTrue(cat.isWhite());
+
+        Field field = Cat.class.getDeclaredField("properties");
+        field.setAccessible(true);
+        Assertions.assertEquals(0x1 | 0x2 | 0x4, field.get(cat));
+    }
+
+    @Test
+    public void test3() throws Exception {
+        Cat cat = new Cat();
+        for (int i = 0; i < 100; ++i) {
+            cat.setCute(true);
+            cat.setFat(true);
+            cat.setWhite(true);
+        }
+        cat.setCute(false);
+        cat.setFat(false);
+        cat.setWhite(false);
+
+        Assertions.assertFalse(cat.isCute());
+        Assertions.assertFalse(cat.isFat());
+        Assertions.assertFalse(cat.isWhite());
+
+        Field field = Cat.class.getDeclaredField("properties");
+        field.setAccessible(true);
+        Assertions.assertEquals(0, field.get(cat));
+    }
+
+    @Test
+    public void test4() throws Exception {
+        Cat cat = new Cat();
+        cat.setFat(true);
+        cat.setWhite(true);
+        cat.setCute(false);
+
+        Assertions.assertTrue(cat.isFat());
+        Assertions.assertTrue(cat.isWhite());
+        Assertions.assertFalse(cat.isCute());
+
+        Field field = Cat.class.getDeclaredField("properties");
+        field.setAccessible(true);
+        Assertions.assertEquals(0x2 | 0x4, field.get(cat));
+    }
+}
